40d772295bd5ebed2c7b77319107478c0d19b789
pkzxs <pkzxs123@gmail.com>
2016-02-27 22:28:07 +0800

    add lost files

---
+++ com.aurora.auweather/com.aurora.auweather/Models/Speed.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Aurora.AuWeather.Models
+{
+    public class Speed
+    {
+        private double kmph;
+
+        public double KMPH
+        {
+            get
+            {
+                return kmph;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                kmph = value;
+            }
+        }
+
+        public double MPS
+        {
+            get
+            {
+                return kmph / 3.6;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                kmph = value * 3.6;
+            }
+        }
+
+        public double Knot
+        {
+            get
+            {
+                return kmph / 1.852;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                kmph = value * 1.852;
+            }
+        }
+
+        public static Speed FromKMPH(double kmph)
+        {
+            var speed = new Speed();
+            speed.KMPH = kmph;
+            return speed;
+        }
+
+        public static Speed FromMPS(double mps)
+        {
+            var speed = new Speed();
+            speed.MPS = mps;
+            return speed;
+        }
+
+        public static Speed FromKnot(double knot)
+        {
+            var speed = new Speed();
+            speed.Knot = knot;
+            return speed;
+        }
+    }
+}
